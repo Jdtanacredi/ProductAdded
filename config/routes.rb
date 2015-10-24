720bf1c8695610a05456dc7b12f7c6c50c6522f7
Justin Tanacredi <jtanacredi@gmail.com>
2015-10-24 14:19:03 -0400

    Test out modal

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'products/index'
   get 'charges/create'
-  root :to => 'mcsubscribe#index'
+  # root :to => 'mcsubscribe#index'
   post 'mcsubscribe/subscribe', to: 'mcsubscribe#subscribe'
-  # root :to =>'mcsubscribe#launch'
+  root :to =>'mcsubscribe#launch'
   get 'mcsubscribe/launch'
 
   resources :products, only: [:index, :show]

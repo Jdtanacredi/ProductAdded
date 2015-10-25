3c3ac5009cffa5270916e3e18ab35f01f6631cef
alexshook <alexandra.shook@gmail.com>
2015-10-25 16:21:26 -0400

    Save address information separately from purchase

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,35 +4,38 @@ class PurchasesController < ApplicationController
       total_price_in_cents: params[:total_price_in_cents],
       stripe_token: params[:stripe_token],
       product: params[:product],
-      quantity: 1,
-      billing_address: {
-        first_name: params[:first_name],
-        last_name: params[:last_name],
-        street_line_1: params[:street_line_1],
-        street_line_2: params[:street_line_2],
-        city: params[:city],
-        postal_code: params[:postal_code],
-        region: params[:region],
-        country: params[:country],
-        vat_number: params[:vat_number],
-        coupon: params[:coupon]
-      },
+      quantity: 1
+    )
+
+    @billing_address = BillingAddress.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      street_line_1: params[:street_line_1],
+      street_line_2: params[:street_line_2],
+      city: params[:city],
+      postal_code: params[:postal_code],
+      region: params[:region],
+      country: params[:country],
+      vat_number: params[:vat_number]
+    )
+
+    @shipping_address = ShippinAddress.new(
       shipping_address: {
-        first_name: params[:first_name],
-        last_name: params[:last_name],
-        street_line_1: params[:street_line_1],
-        street_line_2: params[:street_line_2],
-        city: params[:city],
-        postal_code: params[:postal_code],
-        region: params[:region],
-        country: params[:country],
-        vat_number: params[:vat_number],
-        coupon: params[:coupon]
-      }
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      street_line_1: params[:street_line_1],
+      street_line_2: params[:street_line_2],
+      city: params[:city],
+      postal_code: params[:postal_code],
+      region: params[:region],
+      country: params[:country],
+      vat_number: params[:vat_number],
     )
 
     if @purchase.save
       @purchase.process_payment
+      @billing_address.save
+      @shipping_address.save
 
       # success redirect
     else

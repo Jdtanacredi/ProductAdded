5791c701314e0cf92f43ac767e2431fc2cecbb09
alexshook <alexandra.shook@gmail.com>
2015-10-25 23:54:52 -0400

    Save shipping data

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,38 +5,38 @@ class PurchasesController < ApplicationController
 
   def create
     @purchase = Purchase.new(
-      total_price_in_cents: params[:total_price_in_cents],
-      stripe_token: params[:stripe_token],
-      product: params[:product],
+      total_price_in_cents: params["total_price_in_cents"],
+      stripe_token: params["stripe-token"],
+      product: Product.find_by_name(params["product-name"]),
       quantity: 1
     )
 
     @billing_address = BillingAddress.new(
-      first_name: params[:first_name],
-      last_name: params[:last_name],
-      street_line_1: params[:street_line_1],
-      street_line_2: params[:street_line_2],
-      city: params[:city],
-      postal_code: params[:postal_code],
-      region: params[:region],
-      country: params[:country],
-      vat_number: params[:vat_number]
+      first_name: params["first-name"],
+      last_name: params["last-name"],
+      street_line_1: params["street-line-1"],
+      street_line_2: params["street-line-2"],
+      city: params["city"],
+      postal_code: params["postal-code"],
+      region: params["region"],
+      country: params["country"],
+      vat_number: params["vat-number"],
+      purchase: @purchase
     )
 
     @shipping_address = ShippingAddress.new(
-      first_name: params[:first_name],
-      last_name: params[:last_name],
-      street_line_1: params[:street_line_1],
-      street_line_2: params[:street_line_2],
-      city: params[:city],
-      postal_code: params[:postal_code],
-      region: params[:region],
-      country: params[:country],
-      vat_number: params[:vat_number],
+      first_name: params["first-name"],
+      last_name: params["last-name"],
+      street_line_1: params["street-line-1"],
+      street_line_2: params["street-line-2"],
+      city: params["city"],
+      postal_code: params["postal-code"],
+      region: params["region"],
+      country: params["country"],
+      purchase: @purchase
     )
 
     if @purchase.save
-      @purchase.process_payment
       @billing_address.save
       @shipping_address.save
 

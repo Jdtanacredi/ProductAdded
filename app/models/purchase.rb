40a6572595f038831df3a5d2af05e5ee77b7b8cb
alexshook <alexandra.shook@gmail.com>
2015-10-25 23:41:49 -0400

    Decrease product quantity after each purchase

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,27 +1,13 @@
 class Purchase < ActiveRecord::Base
   belongs_to :product
 
-  validates_presence_of :total_price_in_cents, :stripe_token
+  after_create :decrease_product_quantity
 
-  def process_payment
-    begin
-      charge = Stripe::Charge.create(
-        # if you do the stripe/quaderno thing, i think amount is supposed to be the total amount, including taxes.
-        # then quaderno splits it out for you?
-        :amount => self.total_price_in_cents,
-        :currency => 'usd',
-        :source => self.stripe_token,
-        :description => "Gold001",
-        :capture => true,
-        :metadata => {
-          :tax_name => 'VAT',
-          :tax_rate => 20,
-          :ip_address => request.ip
-        }
-      )
-    rescue Stripe::CardError => e
-      "Your card has been declined."
-    end
+  def decrease_product_quantity
+    existing_quantity = self.product.quantity
+    new_quantity = existing_quantity - 1
+    self.product.quantity = new_quantity
+    self.product.save!
   end
 
 end

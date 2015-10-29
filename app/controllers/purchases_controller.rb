2912bd5dc9f26a68acd105721fcafb785da3978b
alexshook <alexandra.shook@gmail.com>
2015-10-28 21:50:24 -0400

    Add better handling for possible address errors
    Adds more rigorous checks for shipping address. Also sets the shipping
    address to be the same as the billing address as a last resort error
    handler.

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -26,39 +26,43 @@ class PurchasesController < ApplicationController
       purchase: @purchase
     )
 
-    @shipping_address = ShippingAddress.new(
-      first_name: params["first-name"],
-      last_name: params["last-name"],
-      street_line_1: params["shipping-street-line-1"],
-      street_line_2: params["shipping-street-line-2"],
-      city: params["shipping-city"],
-      postal_code: params["shipping-postal-code"],
-      region: params["shipping-region"],
-      country: params["shipping-country"],
-      email_address: params["shipping-email-address"],
-      phone_number: params["phone-number"],
-      purchase: @purchase
-    )
-
     if @purchase.save
       @billing_address.save
 
-      if params["shipping-same"] == "shipping-true"
-        @shipping_address = ShippingAddress.new(
-          first_name: params["first-name"],
-          last_name: params["last-name"],
-          street_line_1: params["street-line-1"],
-          street_line_2: params["street-line-2"],
-          city: params["city"],
-          postal_code: params["postal-code"],
-          region: params["region"],
-          country: params["country"],
-          vat_number: params["vat-number"],
-          email_address: params["email-address"],
-          phone_number: params["phone-number"],
-          purchase: @purchase
-        )
-        @shipping_address.save
+      begin
+        if params["shipping-same"] == "shipping-true"
+          @shipping_address = ShippingAddress.new(
+            first_name: params["first-name"],
+            last_name: params["last-name"],
+            street_line_1: params["street-line-1"],
+            street_line_2: params["street-line-2"],
+            city: params["city"],
+            postal_code: params["postal-code"],
+            region: params["region"],
+            country: params["country"],
+            vat_number: params["vat-number"],
+            email_address: params["email-address"],
+            phone_number: params["phone-number"],
+            purchase: @purchase
+          )
+        elsif params["shipping-same"] == "shipping-false"
+          @shipping_address = ShippingAddress.new(
+            first_name: params["first-name"],
+            last_name: params["last-name"],
+            street_line_1: params["shipping-street-line-1"],
+            street_line_2: params["shipping-street-line-2"],
+            city: params["shipping-city"],
+            postal_code: params["shipping-postal-code"],
+            region: params["shipping-region"],
+            country: params["shipping-country"],
+            email_address: params["email-address"],
+            phone_number: params["phone-number"],
+            purchase: @purchase
+          )
+        end
+        @shipping_address.save!
+      rescue
+        @purchase.set_shipping_address_same_as_billing
       end
 
       redirect_to '/success'

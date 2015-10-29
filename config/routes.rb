53ecb537e850ac8f5c5811bf64d2cd0fb0ef877b
Justin Tanacredi <jtanacredi@gmail.com>
2015-10-28 22:38:14 -0400

    begin frontend sale over styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   post 'mcsubscribe/subscribe', to: 'mcsubscribe#subscribe'
   root :to =>'mcsubscribe#launch'
   get 'mcsubscribe/launch'
-
+  get 'mcsubscribe/index'
   get 'success', to: 'products#success'
 
   resources :products, only: [:index, :show]

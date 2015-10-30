6bdd35e8e9d3891ef2f5b21fb9690af2b073559c
Justin Tanacredi <jtanacredi@gmail.com>
2015-10-30 15:56:51 -0400

    Controller redirect and correct routes to success page

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -9,6 +9,13 @@ class ContestsController < ApplicationController
       email: params["email"],
       name: params["name"],
     )
+
+    if @purchase.save
+      redirect_to '/contest-success'
+    else
+      render json: 'An error has occurred. Please go back and try again.'
+    end
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'contests/index'
-
   post 'mcsubscribe/subscribe', to: 'mcsubscribe#subscribe'
   root :to =>'mcsubscribe#launch'
   get 'mcsubscribe/launch'
   get 'mcsubscribe/index'
   get 'success', to: 'products#success'
+  get 'contests/index'
+  get 'contest-success', to: 'contests#success'
 
   resources :products, only: [:index, :show]
   resources :purchases, only: [:new, :create]
